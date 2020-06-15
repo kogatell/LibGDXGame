b37be0c127be80ea33537828b4713876e9420746
kogatell <kogatell@gmail.com>
2020-06-15 19:58:32 +0200

    background

--- core/src/com/mygdx/game/BackgroundImage.java
+++ core/src/com/mygdx/game/BackgroundImage.java
@@ -1,4 +1,36 @@
 package com.mygdx.game;
 
-public class BackgroundImage {
+import com.badlogic.gdx.Gdx;
+import com.badlogic.gdx.graphics.Texture;
+import com.badlogic.gdx.graphics.g2d.Sprite;
+import com.badlogic.gdx.graphics.g2d.SpriteBatch;
+import com.badlogic.gdx.math.Vector3;
+
+public class BackgroundImage{
+    Vector3 position;
+    int speed;
+    public boolean remove;
+    Texture background1, background2;
+    SpriteBatch batch;
+    public  float yMax, yCoordBg1, yCoordBg2;
+    final int BACKGROUND_MOVE_SPEED = 3;
+    BackgroundImage(float z)
+    {
+        background1 = new Texture(Gdx.files.internal("prev-Space-Background-1.jpg"));
+        background2 = new Texture(Gdx.files.internal("prev-Space-Background-1.jpg")); // identical
+        yMax = 15;
+        yCoordBg1 = yMax*(-1); yCoordBg2 = 0;
+    }
+    /*public void Update(float deltaTime)
+    {
+        backgroundImg.yCoordBg1 += backgroundImg.BACKGROUND_MOVE_SPEED * Gdx.graphics.getDeltaTime();
+        backgroundImg.yCoordBg2 = backgroundImg.yCoordBg1 + backgroundImg.yMax;  // We move the background, not the camera
+        if (backgroundImg.yCoordBg1 >= 0) {
+            backgroundImg.yCoordBg1 = backgroundImg.yMax*(-1); backgroundImg.yCoordBg2 = 0;
+        }
+        batch.begin();
+        batch.draw(backgroundImg.background1, 0, backgroundImg.yCoordBg1);
+        batch.draw(backgroundImg.background2, 0, backgroundImg.yCoordBg2);
+        batch.end();
+    }*/
 }
--- core/src/com/mygdx/game/WorldRenderer.java
+++ core/src/com/mygdx/game/WorldRenderer.java
@@ -12,19 +12,27 @@ import java.util.ArrayList;
 public class WorldRenderer extends WorldController {
     ScrollingBackground background;
     public static OrthographicCamera camera;
+    public static OrthographicCamera camera2;
     SpriteBatch batch;
     WorldController controller;
+    BackgroundImage backgroundImg;
 
     public WorldRenderer(WorldController wc)
     {
         background = new ScrollingBackground();
+        backgroundImg = new BackgroundImage(-1);
         this.controller = wc;
         camera = new OrthographicCamera(Constants.VIEWPORT_WIDTH, Constants.VIEWPORT_HEIGHT);
+        camera2 = new OrthographicCamera(Constants.VIEWPORT_WIDTH/5, Constants.VIEWPORT_HEIGHT/5);
+        camera2.position.set(0,0,0);
+
         camera.position.set(0,0,0);
         camera.update();
         resize(Constants.VIEWPORT_WIDTH, Constants.VIEWPORT_HEIGHT);
+
         //Gdx.graphics.setWindowedMode();
         batch = new SpriteBatch();
+
         background.setSpeedFixed(true);
         if(wc.gameOver)
         {
@@ -51,6 +59,19 @@ public class WorldRenderer extends WorldController {
 
 
 
+
+        backgroundImg.yCoordBg1 += backgroundImg.BACKGROUND_MOVE_SPEED * Gdx.graphics.getDeltaTime();
+        backgroundImg.yCoordBg2 = backgroundImg.yCoordBg1 + backgroundImg.yMax;  // We move the background, not the camera
+        if (backgroundImg.yCoordBg1 >= 0) {
+            backgroundImg.yCoordBg1 = backgroundImg.yMax*(-1); backgroundImg.yCoordBg2 = 0;
+        }
+        batch.draw(backgroundImg.background1, -10, backgroundImg.yCoordBg1, 20, 15);
+        batch.draw(backgroundImg.background2, -10, backgroundImg.yCoordBg2, 20, 15);
+
+        for(Bullet bul: controller.bullets) {
+            bul.render(batch);
+        }
+        background.updateAndRender(Gdx.graphics.getDeltaTime(), batch);
         for(GameObject go: controller.gameObjects) {
             go.getCurrentSprite().setPosition(go.position.x, go.position.y);
             go.getCurrentSprite().setSize(go.width,go.height);
@@ -58,30 +79,28 @@ public class WorldRenderer extends WorldController {
 
         }
 
-        for(Bullet bul: controller.bullets) {
-            bul.render(batch);
-        }
-        background.updateAndRender(Gdx.graphics.getDeltaTime(), batch);
-
         //o el get
 
         batch.end();
         long t2 = System.nanoTime();
         long delt = t2 - t1;
         //Gdx.app.error("TIME", delt+" ns");
-        this.moveCamera(controller.player.position.x, controller.player.position.y);
+        this.moveCamera(controller.player.position.x);
+        camera.update();
 
     }
 
     public void resize(float width, float height)
     {
-        camera.viewportWidth = (Constants.VIEWPORT_HEIGHT/height)*width;
+        camera.viewportWidth = (Constants.VIEWPORT_WIDTH/height)*width;
+        camera2.viewportWidth = (Constants.VIEWPORT_WIDTH/height/2)*width;
+        //camera.viewportHeight= (Constants.VIEWPORT_HEIGHT/width)*height;
         camera.update();
     }
 
-    public void moveCamera(float x, float y)
+    public void moveCamera(float x)
     {
-        camera.position.set(x, y, 0);
+        camera.position.set(x, 0, 0);
     }
 
     public void dispose()
